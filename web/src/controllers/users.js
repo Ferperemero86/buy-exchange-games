33bc6728e040fb0396dad949f96b00d4f4d82516
Fernando Perez <fernandoperezmerono@gmail.com>
2020-04-12 18:16:04 +0100

    Small formatting

--- web/src/controllers/users.js
+++ web/src/controllers/users.js
@@ -29,7 +29,7 @@ router.post("/user", jsonParser, (req, res) => {
   return User
     .where("email", email)
     .fetch()
-    .then ( result => {
+    .then(result => {
       if (result) {
         return res.status(400).json({ userExists: true });  
       }
