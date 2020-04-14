7c93a083ab3be0bd245bea2f646822c605613b6c
Fernando Perez <fernandoperezmerono@gmail.com>
2020-04-14 17:25:49 +0100

    Referencing games to user id in List. Delete all games when list is deleted.

--- web/src/db/migrations/c_create_games_in_list_table_03.js
+++ web/src/db/migrations/c_create_games_in_list_table_03.js
@@ -1,8 +1,9 @@
 exports.up = function (knex) {
     return knex.schema
         .createTable("games_in_list", table => {
+            table.increments("id");
             table.integer("game_id");
-            table.integer("list_id").primary().references("user_id").inTable("lists").onDelete("CASCADE");
+            table.integer("list_id").references("user_id").inTable("lists").onDelete("CASCADE");
             table.string("platform");
             table.string("name");
             table.string("cover");
