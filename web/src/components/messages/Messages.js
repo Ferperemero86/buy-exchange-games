932f0e925bc3ef72fe1ef21ca5e94736db112cb0
Fernando Perez <fernandoperezmerono@gmail.com>
2020-04-28 09:42:24 +0100

    Add new messages

--- web/src/components/messages/Messages.js
+++ web/src/components/messages/Messages.js
@@ -7,6 +7,7 @@ import ValidationError from "../messages/ValidationError";
 const Messages = ({message, page, clearMessage, currentPage, setCurrentPage}) => {
     let msg = null;
     const router = useRouter();
+    console.log(message);
    
     useEffect(() => {
         if (currentPage !== page) {
@@ -86,6 +87,19 @@ const Messages = ({message, page, clearMessage, currentPage, setCurrentPage}) =>
         msg = <p className={`error-message ${page}`}>Could not update list, but game is deleted.</p>;
     }
 
+    if (message.gameUpdated === false) {
+        msg = <p className={`error-message ${page}`}>Could not update Game</p>;
+    }
+
+    if (message.updatedGamesList=== false) {
+        msg = <p className={`error-message ${page}`}>Game status updated for selling but could not load updates. Please refresh browser to see changes </p>;
+    }
+
+    if (message.gameAlreadySelling) {
+        console.log(page);
+        msg = <p className={`error-message ${page}`}>Game already selling</p>;
+    }
+
     /******SUCCESS*****/
 
     if (message.login) {
@@ -113,6 +127,11 @@ const Messages = ({message, page, clearMessage, currentPage, setCurrentPage}) =>
     if (message.listCreated) {
         msg = <p className={`success-message ${page}`}>List created</p>;
         timeOut(clearMessage, false, "3000");
+    }
+
+    if (message.gameForSellUpdated) {
+        msg = <p className={`success-message ${page}`}>Game set for sell</p>;
+        timeOut(router.push, "/account/gameslist", "2000");
     }
 
     return msg;
