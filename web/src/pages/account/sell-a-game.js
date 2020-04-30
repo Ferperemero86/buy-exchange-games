31f517fafe92f3045595a7c2443d5a582e6ef19e
Fernando Perez <fernandoperezmerono@gmail.com>
2020-04-30 12:35:17 +0100

    Add Game display in page

--- web/src/pages/account/sell-a-game.js
+++ web/src/pages/account/sell-a-game.js
@@ -1,29 +1,33 @@
 import React, { useContext } from "react";
+import Link from "next/link";
+import {useRouter} from "next/router";
 import {StoreContext} from "../../utils/store";
 
 import axios from "axios";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowCircleLeft} from '@fortawesome/free-solid-svg-icons';
 
 import Messages from "../../components/messages/Messages";
+import Game from "../../components/games/Game";
 
-const Form = ({status, gameId}) => {
+const Form = ({status, gameId, gameTitle}) => {
     const {gamePrice, setGamePrice} = useContext(StoreContext);
     const {gameCondition, setGameCondition} = useContext(StoreContext);
     const {gameDescription, setGameDescription} = useContext(StoreContext);
     const {gameCurrency, setGameCurrency} = useContext(StoreContext);
-    const {setMessage} = useContext(StoreContext);
+    const {message, setMessage} = useContext(StoreContext);
+    const {currentPage, setCurrentPage} = useContext(StoreContext);
+    const router = useRouter();
 
     const updatePrice = (e) => {
-        console.log(e.currentTarget.value);
         setGamePrice(e.currentTarget.value);
     }
 
     const handleGameCurrency = (e) => {
-       console.log(e.currentTarget.value);
        setGameCurrency(e.currentTarget.value); 
     }
 
     const handleGameCondition = (e) => {
-        console.log(e.currentTarget.value);
         setGameCondition(e.currentTarget.value);
     }
 
@@ -31,39 +35,78 @@ const Form = ({status, gameId}) => {
         setGameDescription(e.currentTarget.value);
     }
 
-    const updateGameStatus = () => {
+    const updateGameStatus = (e) => {
+        e.preventDefault();
+
         axios.post("/api/setgameforsell", {status, gameId, gamePrice,
                                            gameCurrency, gameCondition, gameDescription})
             .then(result => {
                setMessage(result.data);
+               setGamePrice("");
             })
             .catch(err => {
                 setMessage(err.response.data);
             })
     }
 
+    const cancelSelling = (e) => {
+        e.preventDefault();
+        router.push("/account/gameslist")
+    }
+
+    if (gameTitle) {
+        return (
+            <form className="form">
+                <Messages 
+                    page="sellagame" 
+                    message={message} 
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                    clearMessage={setMessage}/>
+                <div className="form-section">
+                    <div className="label"><label>Price</label></div>
+                    <div className="input">
+                        <input type="text" 
+                               className="input" 
+                               onChange={updatePrice}
+                               value={gamePrice} />
+                        <select id="currency" onChange={handleGameCurrency}>
+                            <option value="GBP">£</option>
+                            <option value="EUR">€</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="form-section">
+                <div className="label"><label>Condition</label></div>
+                    <div className="input">
+                        <select id="condition" onChange={handleGameCondition}>
+                            <option value="Grade A">Grade A</option>
+                            <option value="Grade B">Grade B</option>
+                            <option value="Grade C">Grade C</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="form-section">
+                    <div className="label"><label>Description</label></div>
+                    <div className="input">
+                        <textarea className="description"
+                                  onChange={handleGameDescription}></textarea>
+                    </div>
+                </div>
+                <button className="button" 
+                        onClick={updateGameStatus}>Post For Sale</button>
+                <button className="button"
+                        onClick={cancelSelling}>Cancel</button>
+            </form>
+        )
+    }
     return (
-        <div className="game-price">
-            <label className="label">Price</label>
-            <input type="text" 
-                   className="input" 
-                   onChange={updatePrice}
-                   value={gamePrice} />
-            <label className="label">Condition</label>
-            <select id="condition" onChange={handleGameCondition}>
-                <option value="Grade A">Grade A</option>
-                <option value="Grade B">Grade B</option>
-                <option value="Grade C">Grade C</option>
-            </select>
-            <select id="currency" onChange={handleGameCurrency}>
-                <option value="£">GBP</option>
-                <option value="€">EUR</option>
-            </select>
-            <label className="label">Description</label>
-            <textarea className="description"
-                      onChange={handleGameDescription}></textarea>
-            <button className="button" 
-                    onClick={updateGameStatus}>Post For Sell</button>
+        <div className="no-game-result">
+            <h1 className="no-game-heading">No Game selected for Sale</h1>
+            <FontAwesomeIcon icon={faArrowCircleLeft} className="left-arrow" />
+            <Link href="/account/gameslist">
+                <a to="/account/gameslist" className="go-back-link">Go Back</a>
+            </Link>
         </div>
     )
 }
@@ -73,19 +116,15 @@ export async function getServerSideProps({query}) {
 }
 
 const GameToSell = ({query}) => {
-    const {message, setMessage} = useContext(StoreContext);
-    const {currentPage, setCurrentPage} = useContext(StoreContext);
+    const title = query.title;
+    const cover = query.cover;
 
     return (
         <div className="sell-a-game">
-            <Messages 
-                page="sellagame" 
-                message={message} 
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-                clearMessage={setMessage}/>
+            <Game Url={cover} title={title} page="sell-a-game" />
             <Form status={query.status} 
-                gameId={query.id}/>
+                gameId={query.id}
+                gameTitle={title} />
         </div>
     )
 }
