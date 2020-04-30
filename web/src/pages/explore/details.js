d52258938a742e36ac7e7e671589f9b07e8acc9d
Fernando Perez <fernandoperezmerono@gmail.com>
2020-04-30 12:37:41 +0100

    Make cover string changes just when getting not empty data

--- web/src/pages/explore/details.js
+++ web/src/pages/explore/details.js
@@ -21,9 +21,13 @@ const Details = ({query}) => {
     const platform = query.platform;
     const page = query.page;
     const title = query.title;
-    const coverString = query.cover;
-    const cover = coverString.replace("t_thumb", "t_screenshot_med");
     const summary = query.summary;
+    let cover;
+   
+    if (Object.keys(query).length > 0) {
+        const coverString = query.cover;
+        cover = coverString.replace("t_thumb", "t_screenshot_med");
+    }
 
     const addToList = () => {
         const game = query;
@@ -47,39 +51,49 @@ const Details = ({query}) => {
             })
     }
 
-    return (
-        <div className="game-details" key={id}>
-            <Messages 
-                page="details"
-                setCurrentPage={setCurrentPage}
-                currentPage={currentPage}
-                clearMessage={setMessage}
-                message={message} />
-            <div className="header">
-                <div className="go-back-link">
-                    <FontAwesomeIcon icon={faArrowCircleLeft} className="left-arrow" />
-                    <Link href={{ pathname: `/explore/${platform}`, query: {page} }}>
-                        <span className="header-link link">Back To Games</span>
-                    </Link>
+    if (Object.keys(query).length > 0) {
+        return (
+            <div className="game-details" key={id}>
+                <Messages 
+                    page="details"
+                    setCurrentPage={setCurrentPage}
+                    currentPage={currentPage}
+                    clearMessage={setMessage}
+                    message={message} />
+                <div className="header">
+                    <div className="go-back-link">
+                        <FontAwesomeIcon icon={faArrowCircleLeft} className="left-arrow" />
+                        <Link href={{ pathname: `/explore/${platform}`, query: {page} }}>
+                            <span className="header-link link">Back To Games</span>
+                        </Link>
+                    </div>
+                    <h2 className="title">{title}</h2>
                 </div>
-                <h2 className="title">{title}</h2>
+                <img src={`${cover}`} className="cover" />
+                <div className="links">
+                    <ul className="links-list">
+                        <li className="link"
+                            onClick={addToList}>Add to list</li>
+                        <Link href={{ pathname: `/games/users-selling`, 
+                                      query: { id: query.id} }}>
+                            <li className="link">Buy</li>
+                        </Link>
+                        <li className="link">Exchange</li>
+                    </ul>
+                </div>
+                <p className="summary">{summary}</p>
             </div>
-            <img src={`${cover}`} className="cover" />
-            <div className="links">
-                <ul className="links-list">
-                    <li className="link"
-                        onClick={addToList}>Add to list</li>
-                    <Link href={{ pathname: `/games/users-selling`, 
-                                  query: { id: query.id} }}>
-                        <li className="link">Buy</li>
-                    </Link>
-                    <li className="link">Exchange</li>
-                </ul>
-            </div>
-            <p className="summary">{summary}</p>
+        )
+    }
+    return (
+        <div className="no-game-selected">
+            <h1 className="heading">No Game selected</h1>
+            <FontAwesomeIcon icon={faArrowCircleLeft} className="left-arrow" />
+            <Link href="/">
+                <a href="/" className="go-back-link">Go Back</a>
+            </Link>
         </div>
     )
-
 };
 
 
