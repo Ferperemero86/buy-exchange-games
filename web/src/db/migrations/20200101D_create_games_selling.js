86c87992407e7b37b3b242b43425f0804ab1a64a
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-08 11:18:37 +0100

    Add game_id field

--- web/src/db/migrations/20200101D_create_games_selling.js
+++ web/src/db/migrations/20200101D_create_games_selling.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
     return knex.schema
         .createTable("games_selling", table => {
             table.increments("id");
+            table.integer("game_id").notNullable();
             table.integer("list_id").references("user_id").inTable("lists").onDelete("CASCADE");
             table.integer("price").notNullable();
             table.string("currency").notNullable();
