7c4a78b7f456387abe2021cec2c1d62e31863d77
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-08 11:13:18 +0100

    Add separated Game display component

--- web/src/components/games/Game.js
+++ web/src/components/games/Game.js
@@ -1,23 +1,56 @@
-import React from "react";
+import React, {useContext} from "react";
+import {StoreContext} from "../../utils/store";
 
-const Game = ({Url, title, page}) => {
-    let coverURL;
-
-    if (title) {
-        if (page === "sell-a-game") {
-            coverURL = Url.replace("t_thumb", "t_cover_big");
+const RemoveGameButton = ({classEl, gameToRemove, page}) => {
+    const {setGameFromListToExchange} = useContext(StoreContext);
+    const {setGameToExchange} = useContext(StoreContext);
+    const {gameToFind} = useContext(StoreContext);
+    
+    const removeGameFromExchange = () => {
+        if (gameToRemove === "game1") {
+            setGameFromListToExchange([]);
         }
+        if (gameToRemove === "game2") {
+            console.log("game2");
+            setGameToExchange([]);
+        }
+    }
 
-        return (
-            <div className={`game ${page}`}>
-                <div className="cover-container">
-                    <img src={`${coverURL}`} className="cover" />
-                </div>
-                <p className="title">{title}</p>
-            </div>
-        )
+    if (page === "exchange-a-game" && gameToFind || gameToRemove === "game1") {
+        return <button className={`${classEl}`} onClick={removeGameFromExchange}>Remove</button>
     }
     return null;
+}
+
+const Image = ({Url}) => {
+    if (Url) {
+        return <img src={`${Url}`} className="cover" />
+    }
+    return <p className="image-no-available">Image no available</p>
+};
+
+const Game = ({Url, title, page, gameId, platform, gameToRemove}) => {
+    let coverURL;
+    console.log("gamerrr to exchange", gameId);
+    if (title) {
+        if (typeof Url === "string" && Url.indexOf("t_thumb") !== -1) {
+            coverURL = Url.replace("t_thumb", "t_cover_big");
+        } 
+    }
+
+    return (
+        <div className={`game ${page}`} key={gameId}>
+            <RemoveGameButton classEl="remove-game-button" 
+                              gameToRemove={gameToRemove} 
+                              page={page} />
+            <p>{platform}</p>
+            <div className="cover-container">
+                <Image Url={coverURL} />
+            </div>
+            <p className="title">{title}</p>
+        </div>
+    )
+  
 }
 
 export default Game;
