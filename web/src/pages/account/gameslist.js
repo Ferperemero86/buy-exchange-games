d18cd064c7d98a2cc5894fa9f69d260868e631ff
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-08 11:21:18 +0100

    split different components in diferent files.

--- web/src/pages/account/gameslist.js
+++ web/src/pages/account/gameslist.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect} from "react";
-import {useRouter} from "next/router";
+
 import {StoreContext} from "../../utils/store";
 
 import fetch from "node-fetch";
@@ -7,362 +7,10 @@ import axios from "axios";
 
 import DeleteQuestion from "../../components/messages/DeleteQuestion";
 import Messages from "../../components/messages/Messages";
+import GamesSection from "../../components/gameslist/GamesSection";
+import ListInput from "../../components/gameslist/GamesSection";
+import EditList from "../../components/gameslist/GamesSection";
 
-const ListInput = () => {
-    const {createListInputValue, setCreateListInputValue} = useContext(StoreContext);
-    const {createListInput, setCreateListInput} = useContext(StoreContext);
-    const {setEditListMenuActive} = useContext(StoreContext);
-    const {editName} = useContext(StoreContext);
-    const {setListName} = useContext(StoreContext);
-    const {setEditList} = useContext(StoreContext);
-    const {setListCreated} = useContext(StoreContext);
-    const {setMessage} = useContext(StoreContext);
-    const {userId} = useContext(StoreContext);
-    const URL = editName ? "editlistname" : "createlist";
-    const label = URL === "editlistname" ? "Edit Name" : "New List";
-
-
-    const updateInputValue = (e) => {
-        setCreateListInputValue(e.target.value)
-    }
-
-    const sendInputData = (e) => {
-        e.preventDefault();
-        axios({
-            url: `/api/${URL}`,
-            method: "POST",
-            data: {listName: createListInputValue, userId}
-        })
-            .then(result => {
-                const success = result.data;
-
-                if (success.listCreated) {
-                    setCreateListInput(false);
-                    setEditListMenuActive(true);
-                    setEditList(false);
-                    setListCreated(true);
-                    setListName(success.listName)
-                }
-
-                if (success.listNameUpdated) {
-                    setListName(success.listNameUpdated);
-                    setCreateListInput(false);
-                    setEditList(false);
-                    setCreateListInputValue("");
-                }
-            })
-            .catch(err => {
-                const error = err.response.data;
-                setMessage(error);
-            });
-    };
-
-    const cancelEditName = () => {
-        setCreateListInput(false);
-    };
-
-    if (createListInput) {
-        return (
-            <div className="create-list">
-                <form className="create-list-form">
-                    <label className="label">{label}</label>
-                    <input
-                        type="text"
-                        onChange={updateInputValue}
-                        value={createListInputValue}
-                        className="input" />
-                    <button
-                        onClick={sendInputData}
-                        className="button">Send</button>
-                        {URL === "editlistname" && <button 
-                                                        onClick={cancelEditName}
-                                                        className="button">Cancel</button>}
-                </form>
-            </div>
-        )  
-    }
-    return null;
-};
-
-const EditList = () => {
-    const {editListMenuActive} = useContext(StoreContext);
-    const {setAskDeleteList} = useContext(StoreContext);
-    const {setEditName} = useContext(StoreContext);
-    const {editList, setEditList} = useContext(StoreContext);
-    const {setCreateListInput} = useContext(StoreContext);
-
-    const toggleEditMenu = () => {
-        if (!editList) {
-            return setEditList(true)
-        }
-        setEditList(false);
-    };
-
-    const askForListDelete = () => {
-        setAskDeleteList(true);
-    }
-
-    const editListName = () => {
-        setEditName(true);
-        setCreateListInput(true);
-    }
-
-    const List = () => {
-        return (
-            <ul className="list">
-                <li onClick={askForListDelete}>Delete</li>
-                <li onClick={editListName}>Change Name</li>
-            </ul>
-        )
-    }
-
-    if(editListMenuActive) {
-        return (
-            <div className="edit-list">
-                <span
-                    className="span"
-                    onClick={toggleEditMenu}>Edit List</span>
-                {editList === true && <List />}
-            </div>
-        )
-    }
-    return null;
-};
-
-const GameStatus = ({status, gameId}) => {
-    const {setGamesList} = useContext(StoreContext);
-    const {setMessage} = useContext(StoreContext);
-    const {setGameStatus} = useContext(StoreContext);
-    let heading = null;
-   
-    if (status === "selling") {
-        heading = "Selling"    
-    }
-
-    const stopSelling = (e) => {
-        e.preventDefault();
-
-        setGameStatus(true)
-        
-        axios.post("/api/stopselling", {gameId})
-        .then(result => {
-            const gamesList = result.data.gamesList;
-            
-            setGamesList(gamesList);
-        })
-        .catch(err => {
-            setMessage(err.response.data);
-        })
-    }
-   
-    if (heading) {
-        return (
-            <div className="game-status" key={gameId}>
-                <h3 className="heading">{heading}</h3>
-                <button className="button"
-                        onClick={stopSelling}>Stop Selling</button>
-            </div>
-        )
-    }
-    return null;
-}
-
-const GameMenuIcon = ({displayGameMenu, gameId, status}) => {
-
-    if(status === "inList") {
-        return <span className="game-menu-icon"
-             data-key-game-id={gameId} 
-             onClick={displayGameMenu}>....</span>;
-    }
-    return null;
-}
-
-const GameMenu = ({game, hideGameMenu}) => {
-    const {showGameMenu} = useContext(StoreContext);
-    const {gameID} = useContext(StoreContext);
-    const {setShowGameMenu} = useContext(StoreContext);
-    const gameId = game.id;
-    const router = useRouter();
-
-    const goToSellaGame = () => {
-        setShowGameMenu(false);
-
-        router.push({pathname: "/account/sell-a-game", 
-                     query: {id: game.id, 
-                             status: "selling",
-                             title: game.name, 
-                             platform: game.platform, 
-                             cover: game.cover}})
-    }
-   
-    if (showGameMenu && gameID === gameId) {
-        return (
-            <ul className="game-menu">
-                <li className="list-element">
-                    <span className="close-menu-icon" onClick={hideGameMenu}>X</span>
-                </li>
-                
-                    <li className="list-element" 
-                        data-key-game-id={gameId}
-                        onClick={goToSellaGame}>Sell</li>
-               
-                <li className="list-element">Exchange</li>
-            </ul>
-        )
-    }
-    return null;
-}
-
-const DeleteGameIcon = ({game, askForListDelete}) => {
-    return (
-        <div className="delete-icon" 
-             onClick={askForListDelete}
-             data-key-game-id={game.id}>
-            <span className="icon">x</span>
-        </div>
-    )
-}
-
-const Cover = ({Url}) => {
-    return (
-        <div className="cover-container">
-            <img src={`${Url}`} className="cover" />
-        </div>
-    )
-}
-
-const Title = ({title}) => {
-    return (
-        <p className="title">{title}</p>
-    )
-}
-
-const Game = ({platformGames}) => {
-    const {askDeleteGame, setAskDeleteGame} = useContext(StoreContext);
-    const {gameID, setGameID} = useContext(StoreContext);
-    const {setMessage} = useContext(StoreContext);
-    const {setGamesList} = useContext(StoreContext);
-    const {setShowGameMenu} = useContext(StoreContext);
-    let gameId = 0;
-
-    return platformGames.map(game => {
-        const gameCoverString = game.cover;
-        const coverURL = gameCoverString.replace("t_thumb", "t_cover_big");
-        gameId++;
-        const id = gameId + 1;
-    
-
-        const displayGameMenu = (e) => {
-            const gameId = e.currentTarget.getAttribute("data-key-game-id");
-        
-            setGameID(parseInt(gameId));
-            setShowGameMenu(true);
-        }
-
-        const hideGameMenu = () => {
-            setShowGameMenu(false);
-        }
-
-        const deleteGame = () => {
-            axios.post("/api/deletegame", {gameID})
-                .then(result => {
-                    const gamesList = result.data.gamesList;
-                    
-                    setAskDeleteGame(false);
-                
-                    if (Array.isArray(gamesList)) {
-                        setGamesList(gamesList);
-                    } else {
-                        setMessage({internalError: true});
-                    }
-                })
-                .catch(err => {
-                    setAskDeleteGame(false);
-                    setMessage(err.response.data);
-                })
-            
-        };
-
-        const askForListDelete = (e) => {
-            const gameData = e.currentTarget.getAttribute("data-key-game-id");
-
-            setAskDeleteGame(true);
-            setGameID(parseInt(gameData));
-        }
-
-        return (
-            <div className="game" key={id}>
-                <DeleteQuestion 
-                    gameId={game.id}
-                    showQuestion={askDeleteGame} 
-                    action={deleteGame} 
-                    cancelDelete={setAskDeleteGame}
-                    element="game" /> 
-                <GameStatus status={game.status} gameId={game.id} />
-                <GameMenu hideGameMenu={hideGameMenu} 
-                          game={game} /> 
-                <div className="game-header">
-                    <DeleteGameIcon game={game} askForListDelete={askForListDelete} />
-                    <GameMenuIcon displayGameMenu={displayGameMenu} 
-                                  gameId={game.id}
-                                  status={game.status} />
-                </div>
-                <div className="game-info">
-                    <Cover Url={coverURL} />
-                    <Title title={game.name}/>
-                </div>
-            </div> 
-        )
-    })
-}
-
-const Games = ({platformGames}) => {
-    const {setFetchGamesListFromServer} = useContext(StoreContext);
-
-    useEffect(() => {
-        if (platformGames.length > 0) {
-            setFetchGamesListFromServer(false);
-        }
-    })
-
-    return (
-        <div className="gameslist">
-            <Game platformGames={platformGames} />
-        </div>
-    )
-};
-
-const GamesSection = () => {
-    const {gamesList} = useContext(StoreContext);
-    let platform = null;
-    let games = {};
-
-    if (Array.isArray(gamesList)) {
-        gamesList.map(game => {
-            if(!games[game.platform]) {
-                games[game.platform] = [];
-            }
-            games[game.platform].push(game);
-        });
-        
-        return Object.keys(games).map(index => {
-            return games[index].map(game => {
-                if(game.platform !== platform) {
-                    platform = game.platform;
-
-                    return (
-                        <section className="games-list-section" key={platform}>
-                            <h3 className="heading">{platform.toUpperCase()}</h3>
-                            <Games platformGames={games[platform]} />
-                        </section>
-                    )
-                }
-
-            })
-        })
-    }
-    return null;
-};
 
 export async function getServerSideProps(ctx) {
     const userId = await ctx.req.user ? ctx.req.user.id : null;
@@ -376,18 +24,17 @@ export async function getServerSideProps(ctx) {
     const content = await result.json();
 
     let data;
-
+   
     if (await userId) {
         data = content;
     } else {
         data = {login: false};
     }
-
+    
     return { props: {data} };
 }
 
 const UserList = ({data}) => {
-    //{console.log("rendering list.....")}
     const {userId, setUserId} = useContext(StoreContext);
     const {currentPage, setCurrentPage} = useContext(StoreContext);
     const {gamesList, setGamesList} = useContext(StoreContext);
@@ -396,13 +43,14 @@ const UserList = ({data}) => {
     const {setCreateListInputValue} = useContext(StoreContext);
     const {setEditList} = useContext(StoreContext);
     const {message, setMessage} = useContext(StoreContext);
-    const {listName} = useContext(StoreContext);
+    const {listName, setListName} = useContext(StoreContext);
     const {setCreateListInput} = useContext(StoreContext);
-    const {setListName} = useContext(StoreContext);
     const {setEditName} = useContext(StoreContext);
     const {setListDeleted} = useContext(StoreContext);
     const {fetchGamesListFromServer} = useContext(StoreContext);
     const {gameStatus} = useContext(StoreContext);
+    const {gameToExchange, setGameToExchange} = useContext(StoreContext);
+    const {setGameFromListToExchange} = useContext(StoreContext);
 
     const deleteList = () => {
         axios({ 
@@ -427,8 +75,14 @@ const UserList = ({data}) => {
                 setAskDeleteList(false);
             });
     };
-
+   
     useEffect(() => {
+
+        if (Array.isArray(gameToExchange) && gameToExchange.length > 0) {
+            setGameToExchange([]);
+        }
+
+        setGameFromListToExchange(false);
 
         //If not logged redirects to login page
         if (data.login === false) {
@@ -436,6 +90,9 @@ const UserList = ({data}) => {
         }
         
         if (data.listExists === false) {
+            console.log("NO LISTTTT");
+            setGamesList([]);
+            setListName("");
             setCreateListInput(true);
             setEditListMenuActive(false);
         }
@@ -446,7 +103,6 @@ const UserList = ({data}) => {
         
         //Gets data from server
         if (data.gamesList && fetchGamesListFromServer) {
-            
             if (Array.isArray(data.gamesList)) {
                 setGamesList(data.gamesList);
             } else {
@@ -463,32 +119,36 @@ const UserList = ({data}) => {
         //Stops getting data from server.
         //Fetches from client with axios.
         //Updates user id hook if different from coming from server.
-        if (!fetchGamesListFromServer || (data.id !== userId && userId !== null) || !gameStatus ) {
+        if ( (!fetchGamesListFromServer || (data.id !== userId && userId !== null) ) && !gameStatus ) {
             let id;
-            
+
             if (data.id !== userId) {
                 id = data.id;
                 setUserId(data.id);
             } else {
                 id = userId;
             }
-
+           
             setCreateListInput(false);
             setEditListMenuActive(true);
-        
-            //Fetches games and listName when did not get them previously from server.
-            axios("/api/getlist", { method: 'POST', data: {userId: id} })
+           
+            //If list does not exist or user id is different from the last session
+            //Fecth data from the client to update the visual content
+            if (!data.listExists || data.id !== userId) {
+                
+                //Fetches games and listName when did not get them previously from server.
+                axios("/api/getlist", { method: 'POST', data: {userId: id, status: null} })
                     .then(result => {          
                         console.log("fetching elements from client", result);              
                         const gamesList = result.data.gamesList;
                         const listName = result.data.list.result.list_name;
-                
+                    
                         if (Array.isArray(gamesList)) {
                             setGamesList(gamesList);
                         } else {
                             setMessage({couldNotGetList: true});
                         }
-
+                    
                         setCreateListInput(false);
                         setEditListMenuActive(true);
                         setListName(listName);
@@ -498,6 +158,7 @@ const UserList = ({data}) => {
                         setEditListMenuActive(false);
                     })
     
+            }
         }
     }, [gamesList.length]);
 
