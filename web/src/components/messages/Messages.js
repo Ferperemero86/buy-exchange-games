3fb0209f9a78b3c049168eca15b7d24dae4f051c
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-08 11:16:48 +0100

    Add new messages

--- web/src/components/messages/Messages.js
+++ web/src/components/messages/Messages.js
@@ -28,12 +28,12 @@ const Messages = ({message, page, clearMessage, currentPage, setCurrentPage}) =>
         switch (page) {
 
             case "session":
-                msg = <p className={`error-message ${page}`}>User/Password do not match.</p>;
+                msg = <p className={`error-message`}>User/Password do not match.</p>;
 
                 break;
             
             case "details":
-                msg = <p className={`error-message ${page}`}>Please Login first.</p>
+                msg = <p className={`error-message`}>Please Login first.</p>
 
                 break;
 
@@ -45,13 +45,13 @@ const Messages = ({message, page, clearMessage, currentPage, setCurrentPage}) =>
     }
 
     if (message.userExists) {
-        msg = <p className={`error-message ${page}`}>Please choose other username.</p>;
+        msg = <p className={`error-message`}>Please choose other username.</p>;
     }
 
     if (message.listExists) {
         switch(page) {
             case "gameslist":
-                msg = <p className={`error-message ${page}`}>List already exists</p>
+                msg = <p className={`error-message`}>List already exists</p>
 
                 break;
 
@@ -62,7 +62,7 @@ const Messages = ({message, page, clearMessage, currentPage, setCurrentPage}) =>
 
         switch(page) {
             case "details":
-            msg = <p className={`error-message ${page}`}>Please create a List first.</p>
+            msg = <p className={`error-message`}>Please create a List first.</p>
 
             break;
         }
@@ -72,7 +72,7 @@ const Messages = ({message, page, clearMessage, currentPage, setCurrentPage}) =>
 
         switch(page) {
             case "details":
-                msg = <p className={`error-message ${page}`}>Could not add game to the List.</p>
+                msg = <p className={`error-message`}>Could not add game to the List.</p>
 
                 break;
         }
@@ -80,24 +80,27 @@ const Messages = ({message, page, clearMessage, currentPage, setCurrentPage}) =>
     }
 
     if (message.internalError) {
-        msg = <p className={`error-message ${page}`}>Something wrong happened</p>;
+        msg = <p className={`error-message`}>Something wrong happened</p>;
     }
 
     if (message.couldNotGetList) {
-        msg = <p className={`error-message ${page}`}>Could not update list, but game is deleted.</p>;
+        msg = <p className={`error-message`}>Could not update list, but game is deleted.</p>;
     }
 
     if (message.gameUpdated === false) {
-        msg = <p className={`error-message ${page}`}>Could not update Game</p>;
+        msg = <p className={`error-message`}>Could not update Game</p>;
     }
 
     if (message.updatedGamesList=== false) {
-        msg = <p className={`error-message ${page}`}>Game status updated for selling but could not load updates. Please refresh browser to see changes </p>;
+        msg = <p className={`error-message`}>Game status updated for selling but could not load updates. Please refresh browser to see changes </p>;
     }
 
     if (message.gameAlreadySelling) {
-        console.log(page);
-        msg = <p className={`error-message ${page}`}>Game already selling</p>;
+        msg = <p className={`error-message`}>Game already selling</p>;
+    }
+
+    if (message.GameAlreadyExchanging) {
+        msg = <p className={`error-message`}>Game already Exchanging</p>;
     }
 
     /******SUCCESS*****/
@@ -115,22 +118,27 @@ const Messages = ({message, page, clearMessage, currentPage, setCurrentPage}) =>
     }
 
     if (message.register) {
-        msg = <p className={`success-message ${page}`}>Account created</p>;
+        msg = <p className={`success-message`}>Account created</p>;
         timeOut(clearMessage, false, "3000");
     } 
 
     if (message.gameAddedToList) {
-        msg = <p className={`success-message ${page}`}>Game Added to List</p>;
+        msg = <p className={`success-message`}>Game Added to List</p>;
         timeOut(clearMessage, false, "3000");
     }
 
     if (message.listCreated) {
-        msg = <p className={`success-message ${page}`}>List created</p>;
+        msg = <p className={`success-message`}>List created</p>;
         timeOut(clearMessage, false, "3000");
     }
 
     if (message.gameForSellUpdated) {
-        msg = <p className={`success-message ${page}`}>Game set for sell</p>;
+        msg = <p className={`success-message`}>Game set for sell</p>;
+        timeOut(router.push, "/account/gameslist", "2000");
+    }
+
+    if (message.gameSetToExchange) {
+        msg = <p className={`success-message`}>Game Set for Exchange</p>;
         timeOut(router.push, "/account/gameslist", "2000");
     }
 
