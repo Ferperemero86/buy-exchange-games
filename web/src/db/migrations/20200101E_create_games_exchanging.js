8d4938dc3c87cab8d13544296cb5cddf9dbd60b2
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-08 11:19:26 +0100

    Create games exchaging table

---
+++ web/src/db/migrations/20200101E_create_games_exchanging.js
@@ -0,0 +1,18 @@
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("games_exchanging", table => {
+            table.increments("id");
+            table.integer("list_id").notNullable().references("user_id").inTable("lists").onDelete("CASCADE");
+            table.integer("game_1").notNullable();
+            table.integer("game_2").notNullable();
+            table.integer("user_1").notNullable();
+            table.integer("user_2");
+            table.string("status").notNullable();
+            table.string("time");
+        })
+}
+
+exports.down = function (knex) {
+    return knex.schema
+        .dropTable("games_exchanging")
+};
