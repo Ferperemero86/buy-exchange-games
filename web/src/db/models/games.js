0bd2b1674b877039d4557f1033d689532e4807dc
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-25 12:49:43 +0100

    Delete file

--- web/src/db/models/games.js
+++
@@ -1,8 +0,0 @@
-const bookshelf = require("../bookshelf");
-
-const Games = bookshelf.Model.extend({
-    tableName: "games_in_list"
-});
-
-module.exports = Games;
-
--- web/src/db/models/lists.js
+++
@@ -1,7 +0,0 @@
-const bookshelf = require("../bookshelf");
-
-const Lists = bookshelf.Model.extend({
-    tableName: "lists"
-});
-
-module.exports = Lists;
