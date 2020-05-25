1f3a26042a88d07fe25ee1305e1ce729ee0aadc0
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-25 12:47:36 +0100

    Remove automaic delete in database. To be made wuth code instead

--- web/src/db/migrations/20200101E_create_games_exchanging.js
+++ web/src/db/migrations/20200101E_create_games_exchanging.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
     return knex.schema
         .createTable("games_exchanging", table => {
             table.increments("id");
-            table.integer("list_id").notNullable().references("user_id").inTable("lists").onDelete("CASCADE");
+            table.integer("list_id").notNullable();
             table.integer("game_1").notNullable();
             table.integer("game_2").notNullable();
             table.integer("user_1").notNullable();
