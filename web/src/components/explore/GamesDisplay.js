ea2d4e4a3ff92c9385b5a2847e549990db7d6379
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-25 12:02:25 +0100

    Add userReducer logic

--- web/src/components/explore/GamesDisplay.js
+++ web/src/components/explore/GamesDisplay.js
@@ -1,89 +1,104 @@
 import Link from "next/link";
-import React, {useEffect, useContext, useRef } from "react";
-import { StoreContext } from "../../utils/store";
+import React, {useEffect, useContext, useRef} from "react";
+import {useRouter} from "next/router";
+
+import disableScroll from 'disable-scroll';
+
+import {ExploreGamesContext} from "../providers/ExploreGamesProvider";
+
 
 const Cover = ({cover}) => {
     if(cover) {
-        return <img src={`${cover.url}`} />
+        return <img src={`${cover}`} />
     } else {
         return <p>Image no available</p>
     }
 };
 
 const Game = ({game}) => {
-    const {platform} = useContext(StoreContext);
-    const {page} = useContext(StoreContext);
+    const {exploreGames} = useContext(ExploreGamesContext);
+    const page = exploreGames.page;
+    const platform = exploreGames.platform;
     const gameId = game.id;
     const summary = game.summary;
     const longName = game.name; 
     let nameString;
     let cover;
+    const router = useRouter();
     
     if (game.name) {
         nameString = game.name;
     } else {
         nameString = "No Title";
     }
+
     let name = nameString;
+
     if(nameString.length > 19) {
         const shorterName = nameString.substring(0,19);
         name = `${shorterName} ...`;
     }
+
     if (game.cover) {
-        cover = game.cover.url;
+        const bigImage = game.cover.url.replace("t_thumb", "t_cover_big");
+        cover = bigImage;
     } else {
         cover = "";
     }
-    
+
+    const goToDetails = () => {
+        router.push({pathname: "/explore/details", 
+        query: {id: gameId, 
+                title: longName, 
+                platform, 
+                cover, 
+                summary,
+                page } })
+    }
+
+
     return (
-        <Link 
-            href={{ pathname: "/explore/details", 
-                    query: {id: gameId, 
-                            title: longName, 
-                            platform, 
-                            cover, 
-                            summary,
-                            page } }}
-            key={gameId} >
-            <div className="game">
-                <p className="title">{name}</p>
-                <div className="cover">
-                     <Cover cover={game.cover} />
-                </div>
+        <div className="game">
+            <p className="title">{name}</p>
+            <div className="cover">
+                 <Cover cover={cover} />
             </div>
-        </Link>
+            <button className="button" onClick={goToDetails}>Details</button>
+        </div>
     )
     
-};
-
+}
 const Games = () => {
-    const {games} = useContext(StoreContext);
-    const {page} = useContext(StoreContext);
-    let index = parseInt(page);
+    const {exploreGames} = useContext(ExploreGamesContext);
+    let index = parseInt(exploreGames.page);
 
-    if(index > 0) {
+    useEffect(() => {
+        disableScroll.off();
+    })
+
+    if (index > 0) {
         index--;
     }
 
-    if(games[index]) {
-        return games[index].map(game=> {
+    if (exploreGames.games[index]) {
+        return exploreGames.games[index].map(game=> {
             return <Game game={game} key={game.id}/>
         })
-    } else {
-        return null;
-    }
+    } 
+       
+    return null;
+    
 };
 
 const Pagination = () => {
-    const {games} = useContext(StoreContext);
-    const {page} = useContext(StoreContext);
-    const {platform} = useContext(StoreContext);
+    const {exploreGames} = useContext(ExploreGamesContext);
     const listRef = useRef(null);
-
     let indexNav = 0;
-    
+
     const changeStyle = (e) => {
         const children = listRef.current.children;
+
+        disableScroll.on();
 
         Object.keys(children).map(val => {
             const classExists = children[val].classList.contains("selected");
@@ -100,7 +115,7 @@ const Pagination = () => {
     const setFirstPageStyle = () => {
         let selected = "";
 
-        if (page === indexNav) {
+        if (exploreGames.page === indexNav) {
             selected = "selected";
         } else {
             selected = "";
@@ -109,22 +124,24 @@ const Pagination = () => {
         return selected;
     };
 
-    if(games.length > 0) {
+    if(exploreGames.games.length > 0) {
 
         return(  
             <div className="pagination">
                 <ul className="pagination-list" ref={listRef}>
                     {
-                        games.map(() => {
+                        exploreGames.games.map(() => {
                             indexNav++;
                            
                             const selected = setFirstPageStyle();
 
                             return (
                                 <Link 
-                                    href={{ pathname: `/explore/${platform}`, query: {page: indexNav} }} 
+                                    href={{ pathname: `/explore/${exploreGames.platform}`, query: {page: indexNav} }} 
                                     key={indexNav} >
-                                    <li className={`pagination-list-element ${selected}`} onClick={changeStyle}>{indexNav}</li>
+                                    <li className={`pagination-list-element ${selected}`}
+                                        data-page={indexNav}
+                                        onClick={changeStyle}>{indexNav}</li>
                                 </Link>
                             )
                         })
@@ -132,17 +149,18 @@ const Pagination = () => {
                 </ul>
             </div>
         )
-    } else {
-        return null;
-    }
+    } 
+        
+    return null;
+    
 };
 
-const organizeGames = (gamesFromServer) => {
+const organizeGames = (games) => {
     let gamesIndex = 0;
     let paginationGames = [];
 
-    Object.keys(gamesFromServer).map((val) => {    
-        gamesFromServer[val].map((game) => {
+    Object.keys(games).map((val) => {    
+        games[val].map((game) => {
             if(!paginationGames[gamesIndex]) {
                 paginationGames[gamesIndex] = [];
             }
@@ -158,19 +176,20 @@ const organizeGames = (gamesFromServer) => {
     return paginationGames;
 };
 
-const GamesDisplay = ({gamesFromServer}) => {
-    const {games, setGames} = useContext(StoreContext);
-    
-    const result = organizeGames(gamesFromServer);
-        useEffect(() => {
-            setGames(result);
-        }, [games.length]);
+const GamesDisplay = () => {
+    const {exploreGames, dispatchExploreGames} = useContext(ExploreGamesContext);
+    console.log(exploreGames);
+    const result = organizeGames(exploreGames.games);
+
+    useEffect(() => {
+        dispatchExploreGames({type: "UPDATE_GAMES", payload: result});
+    }, [exploreGames.games.length]);
 
     return (
-         <div id="explore-games">
+        <div id="explore-games">
             <Games />
-            <Pagination/>         
-         </div>
+            <Pagination />         
+        </div>
     )
 };
 
