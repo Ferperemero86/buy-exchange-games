8dc22f2ff0cd7303ef7d15670e273aee103b6641
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-25 12:58:53 +0100

    Implement useReducer logic

--- web/src/pages/account/sell-a-game.js
+++ web/src/pages/account/sell-a-game.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, {useContext} from "react";
+
+import Link from "next/link";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowCircleLeft} from '@fortawesome/free-solid-svg-icons';
+
+import {SellGameContext} from "../../components/providers/SellGameProvider";
 
 import Game from "../../components/games/Game";
 import Form from "../../components/sellagame/Form";
@@ -8,16 +14,25 @@ export async function getServerSideProps({query}) {
     return { props: {query} }
 }
 
-const GameToSell = ({query}) => {
-    const title = query.title;
-    const cover = query.cover;
-
+const GameToSell = () => {
+    const {sellGame} = useContext(SellGameContext);
+    const {title, cover, gameId} = sellGame;
+    
+    if (gameId) {
+        return (
+            <div className="sell-a-game">
+                <Game Url={cover} title={title} page="sell-a-game" />
+                <Form />
+            </div>
+        )
+    }
     return (
-        <div className="sell-a-game">
-            <Game Url={cover} title={title} page="sell-a-game" />
-            <Form status={query.status} 
-                gameId={query.id}
-                gameTitle={title} />
+        <div className="no-game-result">
+            <h1 className="no-game-heading">No Game selected for Sale</h1>
+            <FontAwesomeIcon icon={faArrowCircleLeft} className="sell-a-game-left-arrow" />
+            <Link href="/account/gameslist">
+                <a to="/account/gameslist" className="sell-a-game-go-back-link">Go Back</a>
+            </Link>
         </div>
     )
 }
