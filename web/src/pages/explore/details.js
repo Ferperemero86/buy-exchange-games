bfea9e046525c8b2c19b035cd97c252abba4a39b
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-25 12:59:54 +0100

    Organize file in different components

--- web/src/pages/explore/details.js
+++ web/src/pages/explore/details.js
@@ -1,22 +1,52 @@
-import React, {useContext} from "react";
+import React from "react";
 import Link from "next/link";
 import axios from "axios";
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faArrowCircleLeft} from '@fortawesome/free-solid-svg-icons';
 
-import {StoreContext} from "../../utils/store";
-
-import Messages from "../../components/messages/Messages";
 
 export async function getServerSideProps ({query}) {
-
     return {props: {query}}
 }
 
+const Header = ({platform, page, title}) => (
+    <div className="header">
+        <div className="go-back-link">
+            <FontAwesomeIcon icon={faArrowCircleLeft} className="left-arrow" />
+            <Link href={{ pathname: `/explore/${platform}`, query: {page} }}>
+                <span className="header-link link">Back To Games</span>
+            </Link>
+        </div>
+        <h2 className="title">{title}</h2>
+    </div>
+);
+
+const Cover = ({cover}) => (
+    <div className="cover-container">
+        <img src={`${cover}`} className="cover" />
+    </div>
+);
+
+const Links = ({query, addToList}) => (
+    <div className="links">
+        <ul className="links-list">
+            <li className="link"
+                onClick={addToList}>Add to list</li>
+            <Link href={{ pathname: `/games/users-selling`, 
+                          query: { id: query.id} }}>
+                <li className="link">Buy</li>
+            </Link>
+            <li className="link">Exchange</li>
+        </ul>
+    </div>
+);
+
+const Summary = ({summary}) => (
+    <p className="summary">{summary}</p>
+)
+
 const Details = ({query}) => {
-    const {message, setMessage} = useContext(StoreContext);
-    const {currentPage, setCurrentPage} = useContext(StoreContext);
     const id = query.id;
     const platform = query.platform;
     const page = query.page;
@@ -33,57 +63,30 @@ const Details = ({query}) => {
         const game = query;
 
         axios({
-            url: "/api/addgametolist",
+            url: "/api/gamesinlist/game/add",
             method: "POST",
             data: { game }
         })
             .then(result => {
-                const success = result.data;
-
-                setMessage(success);
+                console.log(result);
+                //Show notification
+                
             })
-            .catch(err => {
-                if(err.response) {
-                    const error = err.response.data;
-    
-                    setMessage(error);
-                }
+            .catch(()=> {
+                //Show error notification
             })
     }
 
     if (Object.keys(query).length > 0) {
         return (
             <div className="game-details" key={id}>
-                <Messages 
-                    page="details"
-                    setCurrentPage={setCurrentPage}
-                    currentPage={currentPage}
-                    clearMessage={setMessage}
-                    message={message} />
-                <div className="header">
-                    <div className="go-back-link">
-                        <FontAwesomeIcon icon={faArrowCircleLeft} className="left-arrow" />
-                        <Link href={{ pathname: `/explore/${platform}`, query: {page} }}>
-                            <span className="header-link link">Back To Games</span>
-                        </Link>
-                    </div>
-                    <h2 className="title">{title}</h2>
-                </div>
-                <div className="cover-container">
-                    <img src={`${cover}`} className="cover" />
-                </div>
-                <div className="links">
-                    <ul className="links-list">
-                        <li className="link"
-                            onClick={addToList}>Add to list</li>
-                        <Link href={{ pathname: `/games/users-selling`, 
-                                      query: { id: query.id} }}>
-                            <li className="link">Buy</li>
-                        </Link>
-                        <li className="link">Exchange</li>
-                    </ul>
-                </div>
-                <p className="summary">{summary}</p>
+                <Header platform={platform} 
+                        page={page}
+                        title={title} />
+                <Cover cover={cover}/>
+                <Links query={query}
+                       addToList={addToList} />
+                <Summary summary={summary} />
             </div>
         )
     }
@@ -97,7 +100,6 @@ const Details = ({query}) => {
         </div>
     )
 };
-
 
 export default Details;
 
