5407c725cc0f1a56b38381673e34b49a8b4a0988
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-25 12:53:37 +0100

    Add removeGameButton as a independent component in GameToExchange

--- web/src/pages/account/exchange-a-game.js
+++ web/src/pages/account/exchange-a-game.js
@@ -1,12 +1,12 @@
-import React, {useContext, useEffect} from "react";
-import {StoreContext} from "../../utils/store";
+import React from "react";
 
 import {faArrowsAltH} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-import Messages from "../../components/messages/Messages";
 import FirstGame from "../../components/exchangeagame/FirstGame";
 import SecondGame from "../../components/exchangeagame/SecondGame";
+import CancelButton from "../../components/exchangeagame/CancelButton";
+import RemoveGameButton from "../../components/exchangeagame/RemoveGameButton";
 import ExchangeConfirmationButton from "../../components/exchangeagame/ExchangeConfirmationButton";
 import SearchWindow from "../../components/exchangeagame/SearchWindow";
 
@@ -15,39 +15,30 @@ export async function getServerSideProps ({query}) {
     return { props: {query} };
 }
 
-const GameToExchange = ({query}) => {
-    const {message, setMessage} = useContext(StoreContext);
-    const {currentPage, setCurrentPage} = useContext(StoreContext);
-    const {gameFromListToExchange, setGameFromListToExchange} = useContext(StoreContext);
-    const game = query;
-    const title = query.title;
-    const cover = query.cover;
-    
-    useEffect(() => {
-        if (!gameFromListToExchange) {
-            setGameFromListToExchange(game);
-        }
-    }, [setGameFromListToExchange.length])
 
+const GameToExchange = () => {
     return (
         <div className="exchange-a-game">
             <SearchWindow />
             <div className="games">
-                <FirstGame cover={cover} 
-                           title={title} />
-                <Messages 
-                    page="exchange-a-game" 
-                    message={message} 
-                    currentPage={currentPage}
-                    setCurrentPage={setCurrentPage}
-                    clearMessage={setMessage}/>
+                <div className="first-game">
+                    <RemoveGameButton 
+                        classEl="remove-game-button" 
+                        gameToRemove="game1" />
+                    <FirstGame />
+                </div>
                 <FontAwesomeIcon icon={faArrowsAltH} className="exchange-arrow" />
-                <ExchangeConfirmationButton game1={query.id} />
-                <SecondGame />
+                <CancelButton />
+                <ExchangeConfirmationButton />
+                <div className="second-game">
+                    <RemoveGameButton 
+                        classEl="remove-game-button" 
+                        gameToRemove="game2" />
+                    <SecondGame />
+                </div>
             </div>
         </div>
     )
-
 }
 
 export default GameToExchange;
