366ecf7fdc0d67dc8acb65b55044821939f5f733
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-25 12:45:14 +0100

    Create a short games_in_list table. Rest of content to be fetched in other table

--- web/src/db/migrations/20200101C_create_games_in_list_table.js
+++ web/src/db/migrations/20200101C_create_games_in_list_table.js
@@ -1,13 +1,10 @@
 exports.up = function (knex) {
     return knex.schema
         .createTable("games_in_list", table => {
-            table.increments("id");
+            table.increments("id").notNullable();
+            table.integer("list_id").notNullable();
             table.integer("game_id").notNullable();
-            table.integer("list_id").references("user_id").inTable("lists").onDelete("CASCADE");
-            table.string("platform");
-            table.string("name").notNullable();
             table.string("status").defaultTo("inList").notNullable();
-            table.string("cover");
         })
 }
 
