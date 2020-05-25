8906d3075a21eec0a2b7fb03a9fb60bb3ac74a25
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-25 12:33:49 +0100

    Add useReducer logic

--- web/src/components/sellagame/Form.js
+++ web/src/components/sellagame/Form.js
@@ -1,24 +1,21 @@
 import React, { useContext } from "react";
-import Link from "next/link";
+
 import {useRouter} from "next/router";
-import {StoreContext} from "../../utils/store";
+import {SellGameContext} from "../providers/SellGameProvider";
 
 import axios from "axios";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faArrowCircleLeft} from '@fortawesome/free-solid-svg-icons';
 
-import Messages from "../../components/messages/Messages";
 
 const Price = () => {
-    const {gamePrice, setGamePrice} = useContext(StoreContext);
-    const {setGameCurrency} = useContext(StoreContext);
-
+    const {sellGame, dispatchSellGame} = useContext(SellGameContext);
+    const {gamePrice} = sellGame;
+    
     const updatePrice = (e) => {
-        setGamePrice(e.currentTarget.value);
+        dispatchSellGame({type: "UPDATE_GAME_PRICE", payload: e.currentTarget.value})
     }
 
     const handleGameCurrency = (e) => {
-       setGameCurrency(e.currentTarget.value); 
+        dispatchSellGame({type: "UPDATE_GAME_CURRENCY", payload: e.currentTarget.value})
     }
 
     return (
@@ -40,10 +37,10 @@ const Price = () => {
 }
 
 const Condition = () => {
-    const {setGameCondition} = useContext(StoreContext);
+    const {dispatchSellGame} = useContext(SellGameContext)
 
     const handleGameCondition = (e) => {
-        setGameCondition(e.currentTarget.value);
+        dispatchSellGame({type: "UPDATE_GAME_CONDITION", payload: e.currentTarget.value})
     }
 
     return (
@@ -61,10 +58,10 @@ const Condition = () => {
 }
 
 const Description = () => {
-    const {setGameDescription} = useContext(StoreContext);
+    const {dispatchSellGame} = useContext(SellGameContext);
 
     const handleGameDescription = (e) => {
-        setGameDescription(e.currentTarget.value);
+        dispatchSellGame({type: "UPDATE_GAME_DESCRIPTION", payload: e.currentTarget.value})
     }
 
     return (
@@ -79,32 +76,24 @@ const Description = () => {
 }
 
 
-const Form = ({status, gameId, gameTitle}) => {
-    const {gamePrice, setGamePrice} = useContext(StoreContext);
-    const {setGamesList} = useContext(StoreContext);
-    const {gameCondition} = useContext(StoreContext);
-    const {gameDescription} = useContext(StoreContext);
-    const {gameCurrency} = useContext(StoreContext);
-    const {message, setMessage} = useContext(StoreContext);
-    const {currentPage, setCurrentPage} = useContext(StoreContext);
+const Form = () => {
+    const {sellGame, dispatchSellGame} = useContext(SellGameContext);
+    const {gamePrice, gameId, status, 
+          gameCondition, gameDescription, gameCurrency} = sellGame;
     const router = useRouter();
-
-   
+    
     const updateGameStatus = (e) => {
         e.preventDefault();
-
-        axios.post("/api/setgameforsell", {status, gameId, gamePrice,
-                                           gameCurrency, gameCondition, gameDescription})
-            .then(result => {
-               const message = result.data.message;
-               const gamesList = result.data.gamesList;
-
-               setMessage(message);
-               setGamesList(gamesList);
-               setGamePrice("");
+    
+        axios.post("/api/gamesinlist/game/sell", 
+                    {status, gameId, gamePrice,
+                    gameCurrency, gameCondition, gameDescription })
+            .then(() => {
+               dispatchSellGame({type: "UPDATE_GAME_PRICE", payload: ""});
+               router.push("/account/gameslist");
             })
-            .catch(err => {
-                if (err.response) { setMessage(err.response.data) }
+            .catch(() => {
+        
             })
     }
 
@@ -113,33 +102,16 @@ const Form = ({status, gameId, gameTitle}) => {
         router.push("/account/gameslist")
     }
 
-    if (gameTitle) {
-        return (
-            <form className="form">
-                <Messages 
-                    page="sellagame" 
-                    message={message} 
-                    currentPage={currentPage}
-                    setCurrentPage={setCurrentPage}
-                    clearMessage={setMessage}/>
-                <Price />
-                <Condition />
-                <Description />
-                <button className="button" 
-                        onClick={updateGameStatus}>Post For Sale</button>
-                <button className="button"
-                        onClick={cancelSelling}>Cancel</button>
-            </form>
-        )
-    }
     return (
-        <div className="no-game-result">
-            <h1 className="no-game-heading">No Game selected for Sale</h1>
-            <FontAwesomeIcon icon={faArrowCircleLeft} className="left-arrow" />
-            <Link href="/account/gameslist">
-                <a to="/account/gameslist" className="go-back-link">Go Back</a>
-            </Link>
-        </div>
+        <form className="form">
+            <Price />
+            <Condition />
+            <Description />
+            <button className="button" 
+                    onClick={updateGameStatus}>Post For Sale</button>
+            <button className="button"
+                    onClick={cancelSelling}>Cancel</button>
+        </form>
     )
 }
 
