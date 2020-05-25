087be5865d4b690cac53394dbb8885daa9219204
Fernando Perez <fernandoperezmerono@gmail.com>
2020-05-25 12:50:19 +0100

    getAdmin function to be added later

--- web/src/db/models/user.js
+++ web/src/db/models/user.js
@@ -1,14 +1,14 @@
 const bookshelf = require("../bookshelf");
 
 const User = bookshelf.Model.extend({
-  tableName: "users",
-  getAdmin: function(user) {
-    if (user) {
-      return user.get("isAdmin");
-    } else {
-      return null;
-    }
-  },
+  tableName: "users"
+  //getAdmin: function(user) {
+  //  if (user) {
+  //    return user.get("isAdmin");
+  //  } else {
+  //    return null;
+  //  }
+  //},
 });
 
 module.exports = User;
