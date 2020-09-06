abfc264cce0edceb46a90daa06b7930a8dee822b
Fernando Perez <fernandoperezmerono@gmail.com>
2020-09-06 10:32:37 +0100

    Add  centralized user role permissions

--- web/src/controllers/acl.js
+++ web/src/controllers/acl.js
@@ -1,24 +1,35 @@
-function middleware() { //{TargetModel, action}
-  //const CanCan = require("cancan");
-  //const cancan = new CanCan();
-  //const { allow, can } = cancan;
-  //const User = require("../db/models/user");
-//
+function middleware(Model, TargetModel, action) { //{TargetModel, action}
+  const CanCan = require("cancan");
+  const cancan = new CanCan();
+  const { allow, can } = cancan;
+  const User = require("../db/models/user");
+  const AdminUser = require("../db/models/admin-user");
+
   return function (req, res, next) {
-  //  const userModelInstance = new User(req.user);
-  //  const targetModelInstance = new Targetmodel(req.user);
-//
-  //  allow(User, ["edit", "delete", "create", "save"], User, (user, target) => {
-  //    if (user.id === target.id && !user.getAdmin(req.user)) {
-  //      return true;
-  //    } else {
-  //      return false;
-  //    }
-  //  });
-//
-  //  if (!can(userModelInstance, action, targetModelInstance)) {
-  //    res.status(403).send({ authError: "User not auhorised" });
-  //  }
+    const modelInstance = new Model(req.user);
+    const targetModelInstance = new TargetModel(req.user);
+   
+    allow (AdminUser, "manage", User, (user) => {
+      if (user.get("isAdmin")) {
+        return true;
+      }
+      return false;
+    });
+
+    allow (User, ["edit", "delete", "create", "save", "read"], User, (user, target) => {
+      if (user.id === target.id) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    console.log("CAN USER?", can(modelInstance, action, targetModelInstance));
+
+    if (!can(modelInstance, action, targetModelInstance)) {
+      return res.status(403).json({ authError: true });
+    }
+    console.log("NEXT!");
     return next();
  };
 }
