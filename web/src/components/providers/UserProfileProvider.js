4584e547677299f8ad513da465e1ed2631c61c94
Fernando Perez <fernandoperezmerono@gmail.com>
2020-09-06 10:30:18 +0100

    Centralice funtions in provider

--- web/src/components/providers/UserProfileProvider.js
+++ web/src/components/providers/UserProfileProvider.js
@@ -1,18 +1,21 @@
-import React, {createContext, useReducer, useMemo} from "react";
+import React, {createContext, useReducer, useMemo, useEffect} from "react";
 
 import {userProfileReducer} from "../../utils/reducers";
 import {sendDataFromClient} from "../../utils/API";
+import {useRouter} from "next/router";
 
 export const UserProfileContext = createContext();
 
 
 const UserProfileProvider = ({children, pageProps}) => {
-    const {nickName, country, city, picture} = pageProps.profile.profile;
-
-    const saveFieldValue = (fieldValue, userId, fieldName) => {
-        return sendDataFromClient("/api/user/profile/save", {fieldValue, userId, fieldName});
-    }
-
+    const router = useRouter();
+    const {login, userQueryId, userId, countryCode} = pageProps;
+    const cities = pageProps.cities ? pageProps.cities.cities : [];
+    const nickName = pageProps.profile ? pageProps.profile.profile.nickName : "";
+    const country = pageProps.profile ? pageProps.profile.profile.country : "";
+    const city = pageProps.profile ? pageProps.profile.profile.city : "";
+    const picture = pageProps.profile ? pageProps.profile.profile.picture : "";
+    
     const initialValues = {
         messageForm: false,
         editField: false,
@@ -20,22 +23,87 @@ const UserProfileProvider = ({children, pageProps}) => {
         fieldValue: "",
         nickName,
         country,
+        countryCode,
         city,
-        cities: "",
+        cities,
         profileImage: "",
-        profileImageUrl: picture
-    }
-    
+        profileImageUrl: picture,
+        userQueryId,
+        userId
+    };
+
     const [userProfileState, dispatchUserProfile] = useReducer(userProfileReducer, initialValues);
 
     const userProfile = useMemo(() => {
         return userProfileState
-    }, [userProfileState])
+    }, [userProfileState]);
+
+    const showButtons = (e) => {
+        e.preventDefault();
+        
+        const field = e.currentTarget.getAttribute("data");
+        dispatchUserProfile({type: "SHOW_EDIT_PROFILE_FIELD", payload: field});
+    }
+
+    const cancelEdit = () => {
+        dispatchUserProfile({type: "SHOW_EDIT_PROFILE_FIELD", payload: false});
+    };
+
+    const updateField = (e) => {
+        const data = e.currentTarget.getAttribute("data");
+        const dataArray = data.split(",");
+        const fieldName = dataArray[0];
+        const fieldValue = dataArray[1];
+
+        dispatchUserProfile({type: "SHOW_EDIT_PROFILE_FIELD", payload: false});
+        const fieldUpdated = sendDataFromClient("/api/user/profile/save", {fieldValue, userId, userQueryId, fieldName});     
+        
+        fieldUpdated.then(result=> {
+            if(result.authError) { router.push("/not-authorised") } 
+            if (result.fieldName === "Nickname") {
+                dispatchUserProfile({type: "UPDATE_NICKNAME", payload: result.fieldValue});
+            }
+            if (result.fieldName === "Country") {
+                dispatchUserProfile({type: "UPDATE_COUNTRY", payload: result.fieldValue});
+            }
+        })
+    };
+
+    const updateFieldValue = (e) => {
+        const value = e.currentTarget.value;
+       
+        dispatchUserProfile({type: "UPDATE_FIELD_VALUE", payload: value});
+        dispatchUserProfile({type: "UPDATE_NICKNAME", payload: value});
+    };
+
+    const updateImage = (e) => {
+        const image = e.currentTarget.files[0];
+
+        dispatchUserProfile({type: "UPDATE_PROFILE_IMAGE", payload: image});
+    };
+
+    const showMessageForm = () => {
+        dispatchUserProfile({type: "SHOW_MESSAGE_FORM", payload: true});
+    };
+
+    const closeMessageForm = () => {
+        dispatchUserProfile({type: "SHOW_MESSAGE_FORM", payload: false})
+    };
+
+    useEffect(() => {
+        if (login === false) { router.push("/account/login") }
+    });
        
     return <UserProfileContext.Provider value={{
         userProfile, 
         dispatchUserProfile,
-        saveFieldValue
+        updateField,
+        cancelEdit,
+        showButtons,
+        updateFieldValue,
+        updateImage,
+        showMessageForm,
+        closeMessageForm
     }}>{children}</UserProfileContext.Provider>
 
 }
