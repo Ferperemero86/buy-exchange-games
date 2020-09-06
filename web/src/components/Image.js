cd1d79fc1a691856ac312b88189343538153f8d0
Fernando Perez <fernandoperezmerono@gmail.com>
2020-09-06 10:27:29 +0100

    Do not display anything if there no image to show

--- web/src/components/Image.js
+++ web/src/components/Image.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Image = ({className, Url}) => (
-    <img className={className} src={Url} />
-)
+const Image = ({className, Url}) => {
+    if (!Url || Url === "not selected" || "") { return null }
+
+    return (
+        <img className={className} src={Url} />
+    )
+}
 
 export default Image;
