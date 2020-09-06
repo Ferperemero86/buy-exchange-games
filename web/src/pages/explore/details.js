fdb215f54d32a005f10a7d186fb4a06ba050b160
Fernando Perez <fernandoperezmerono@gmail.com>
2020-09-06 11:09:02 +0100

    Add reusable components

--- web/src/pages/explore/details.js
+++ web/src/pages/explore/details.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Link from "next/link";
-import axios from "axios";
+import {sendDataFromClient} from "../../utils/API";
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faArrowCircleLeft} from '@fortawesome/free-solid-svg-icons';
+
+import Heading from "../../components/Heading";
+import List from "../../components/List";
+import ListElement from "../../components/ListElement";
+import Image from "../../components/Image";
+import Paragraph from "../../components/Paragraph";
+import Anchor from "../../components/Anchor";
 
 
 export async function getServerSideProps ({query}) {
@@ -12,33 +19,44 @@ export async function getServerSideProps ({query}) {
 
 const Header = ({title}) => (
     <div className="header">
-        <h2 className="title">{title}</h2>
+        <Heading 
+         className="title"
+         type="h2"
+         text={title} />
     </div>
 );
 
 const Cover = ({cover}) => (
     <div className="cover-container">
-        <img src={`${cover}`} className="cover" />
+        <Image 
+         Url={`${cover}`} 
+         className="cover" />
     </div>
 );
 
 const Links = ({query, addToList}) => (
     <div className="links">
-        <ul className="links-list">
-            <li className="link"
-                onClick={addToList}>Add to list</li>
-            <Link href={{ pathname: `/games/users-selling`, 
-                          query: { id: query.id} }}>
-                <li className="link">Buy</li>
-            </Link>
-            <li className="link">Exchange</li>
-        </ul>
+        <List className="links-list">
+            <ListElement 
+             className="link"
+             onClick={addToList}>Add to list</ListElement>
+            <ListElement className="link">
+                <Link href={{ pathname: `/games/users-selling`, 
+                              query: { id: query.id} }}>
+                       <a href=""
+                          className="details-buy-link">Buy</a>
+                </Link>
+            </ListElement>
+            <ListElement className="link">Exchange</ListElement>
+        </List>
     </div>
 );
 
 const Summary = ({summary}) => (
-    <p className="summary">{summary}</p>
-)
+    <Paragraph 
+     className="summary" 
+     text={summary} />
+);
 
 const Details = ({query}) => {
     const id = query.id;
@@ -56,20 +74,14 @@ const Details = ({query}) => {
     const addToList = () => {
         const game = query;
 
-        axios({
-            url: "/api/gamesinlist/game/add",
-            method: "POST",
-            data: { game }
-        })
+        sendDataFromClient("/api/gamesinlist/game/add", {game})
             .then(result => {
-                console.log(result);
-                //Show notification
-                
+                console.log("GAME ADDED", result)
             })
-            .catch(()=> {
-                //Show error notification
+            .catch(err => {
+                console.log("ERROR", err);
             })
-    }
+    };
 
     if (Object.keys(query).length > 0) {
         return (
@@ -86,10 +98,16 @@ const Details = ({query}) => {
     }
     return (
         <div className="no-game-selected">
-            <h1 className="heading">No Game selected</h1>
-            <FontAwesomeIcon icon={faArrowCircleLeft} className="left-arrow" />
+            <Heading 
+             className="heading"
+             text="No Game Selected" />
+            <FontAwesomeIcon 
+             icon={faArrowCircleLeft} 
+             className="left-arrow" />
             <Link href="/">
-                <a href="/" className="go-back-link">Go Back</a>
+                <Anchor 
+                 Url="/" 
+                 className="go-back-link">Go Back</Anchor>
             </Link>
         </div>
     )
