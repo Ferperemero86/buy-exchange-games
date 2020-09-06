ff873f3e6dbd33c7c2d08974509d24ab22232ced
Fernando Perez <fernandoperezmerono@gmail.com>
2020-09-06 12:37:32 +0100

    Change format

--- web/src/components/Option.js
+++ web/src/components/Option.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Option = ({value, children, onClick}) => (
-    <option value={value} onClick={onClick}>{children}</option>
+    <option value={value} 
+            onClick={onClick}>{children}</option>
 )
 
 export default Option;
