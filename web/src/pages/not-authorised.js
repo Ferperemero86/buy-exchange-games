36255be784361510c230b50250161b1efe1b9c47
Fernando Perez <fernandoperezmerono@gmail.com>
2020-09-06 10:37:16 +0100

    Create a not-authorised page content display

---
+++ web/src/pages/not-authorised.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Heading from "../components/Heading";
+
+const NotAuthorised = () => (    
+    <Heading
+     className="not-authorised-message"
+     text="User not Authorised"
+     type="h1" />
+)
+
+export default NotAuthorised;
